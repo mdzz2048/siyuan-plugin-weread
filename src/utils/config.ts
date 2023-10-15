aa4423030520cbb9be5b0fd7ff0d728798c870af
mdzz2048 <2434206717@qq.com>
2023-10-15 16:55:27 +0800

    feat: :sparkles: 添加更新插件配置方法

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,27 @@
 import { Plugin } from "siyuan";
+import { client } from "../api/siyuan";
+import { WereadConfig } from "../types/config";
 
-export function getPluginConfig(plugin_name: string, storageName = "config") {
+export function getPluginConfig(pluginName: string, storageName = "config") {
     const plugins: Plugin[] = window["siyuan"]["ws"]["app"]["plugins"];
     for(let index = 0; index < plugins.length; index++) {
         const plugin = plugins[index];
-        if (plugin.name === plugin_name) {
+        if (plugin.name === pluginName) {
             return plugin.data[storageName];
         }
     }
+}
+
+export function updatePluginConfig(pluginName: string, storageName = "config", file: File | BlobPart) {
+    client.putFile({
+        path: `/data/storage/petal/${pluginName}/${storageName}`,
+        isDir: false,
+        modTime: Date.now(),
+        file: file
+    });
+}
+
+export function getConfigBlob(config: WereadConfig): Blob {
+    const jsonStr = JSON.stringify(config);
+    return new Blob([jsonStr], { type: 'application/json' });
 }
