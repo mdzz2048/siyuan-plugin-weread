3d71f09125688d1c620be3f00ac66077ef0178f1
mdzz2048 <2434206717@qq.com>
2023-10-15 20:07:19 +0800

    feat: :sparkles: 单本导入UI

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Library from "./tab-library/Library.svelte";
-import CardView from "./tab-import/CardView.svelte";
-import Settings from "./pannel-setting/Setting.svelte";
+import CardView from "./import/CardView.svelte";
+import Settings from "./pannel-setting/Setting.svelte"
+import PanelSync from "./PanelSync.svelte";
 
 export {
     Library, 
     CardView, 
-    Settings
+    Settings,
+    PanelSync
 }
